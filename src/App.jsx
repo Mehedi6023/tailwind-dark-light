9506c4ee324dce13c24ec95169d83557c03abead
Mehedi6023 <quazimehedihasanovi@gmail.com>
2025-06-06 14:16:29 +0600

    created files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from './components/Hero'
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-neutral-100 relative'>
+      <Hero />
+    </div>
+  )
+}
+
+export default App
